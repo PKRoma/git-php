628b2607f8bd0c1538802eed19cd543879262827
tiger <tiger@tiger-desktop.(none)>
2011-05-31 14:13:56 +0200

    - modified css, added some new classes - improved creation of a repository, added some more info on how to connect the remote with the local repository - added index.php which redirects to git.php

--- manage.php
+++ manage.php
@@ -15,11 +15,11 @@ if(isset($_GET['action']) && $_GET['action']=="create")
     $pName = $_POST['project_name'].'.git';
     if(is_dir($repo_directory.'/'.$pName))
     {
-      die("A project with this name already exists");
+      error("A project with this name already exists");
     }
     else if(strlen($pName) == 0)
     {
-      die("Empty project name!");
+      error("Empty project name!");
     }
 // create the project
     exec("git init --bare ".$repo_directory.'/'.$pName);
@@ -37,24 +37,60 @@ if(isset($_GET['action']) && $_GET['action']=="create")
     }
     fwrite($fh, "User:".$user." IP:".$_SERVER['REMOTE_ADDR']." Action: Create repo:".$pName."\n");
     fclose($fh);
-    echo "Created project ".$pName;
+    info_text();
   }
   else
   {
 ?>
+<div align="center">
 <div class="githead">GIT project creation</div>
 <form action="manage.php?action=create" method="POST">
 <table><TR><TD align="center">Project name </TD>
 <TD><INPUT type="text" name="project_name">.git</TD></TR>
 <TR><TD>Description:</TD>
-<TD><textarea name="description"></textarea></TD></TR>
+<TD><textarea cols="30" name="description"></textarea></TD></TR>
 <TR><TD align="center" colspan="2"><INPUT type="submit" value="Create project"></TD></TR></table>
 </form>
+</div>
 
 <?php
   }
 html_footer();
 }
 
+function info_text()
+{
+  global $pName;
+  global $server_name;
+  global $repo_http_relpath;
+  ?>
+  <div align="center">
+    <div class="githead">Created GIT project <?php echo $pName?></div>
+    <div class="content">
+      <p>So, what's next?<br />
+      You can connect your local repository to the remote repository just created as follows:</p>
 
+      <h2>Add the remote repository</h2>
+      <p class="commandline">$&gt; git remote add origin http://<?php if(isset($_SERVER['REMOTE_USER'])){ echo $_SERVER['REMOTE_USER']; } else {echo "&lt;username&gt;";} ?>@<?php echo $server_name.$repo_http_relpath.$pName ?></p>
+
+      <h2>Push the local master branch to the created remote repository</h2>
+      <p class="commandline">$&gt; git push origin master</p>
+    </div>
+  </div>
+<?php
+  html_footer();
+}
+
+function error($text)
+{
+  ?>
+  <div align="center">
+  <div class="githead">Error</div>
+  <table><tr><td><?php echo $text;?>
+  </td></tr></table>
+  </div>
+  <?php
+  html_footer();
+  die();
+}
 ?>
